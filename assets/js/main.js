9fbe7a8d6b658b844e1268af9a758851a430ea98
Eliot Lash <fadookie@gmail.com>
2025-02-02 22:02:28 -0800

    finish permalink structure and JS to switch pages from drop-down

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,16 @@
 // @ts-check
 
 function onReady() {
-  initCategoryPicker();
-}
-
-const categoryClassPrefix = "portfolio_thumb_category_";
-
-/** @type Array<Element> | null */
-let thumbItems = null;
-
-function initCategoryPicker() {
-  thumbItems = Array.from(document.getElementsByClassName('portfolio_thumb_item'));
-  // const allClasses = Array.from(new Set(thumbItems.flatMap(x => Array.from(x.classList))));
-  // const categories = allClasses.filter(x => x.startsWith(categoryClassPrefix))
-  //   .map(x => x.replace(categoryClassPrefix, ""));
-
-  // const categoryPicker = /** @type HTMLSelectElement */ (document.getElementById("category_picker")); 
-  // categories.forEach(category => {
-  //   categoryPicker.add(new Option(category));
-  // });
 }
 
 function selectCategory(evt) {
-  if (thumbItems == null) { throw new Error("Expected thumbItems to not be null."); }
-  const isVisibleClass = "is_visible";
+  // const isVisibleClass = "is_visible";
   const selectElement = evt.target;
+  /** @type String */
   const value = selectElement.value;
-  const selectedCategoryClass = `${categoryClassPrefix}${value}`;
-
-  const [selectedItems, unselectedItems] = (() => {
-    if (value == 'All') return [thumbItems, []];
-    const selectedItems = thumbItems.filter(x => x.classList.contains(selectedCategoryClass));
-    const unselectedItems = thumbItems.filter(x => !x.classList.contains(selectedCategoryClass));
-    return [selectedItems, unselectedItems];
+  const newLocation = (() => {
+    if (value == 'Featured') return '/';
+    return `/portfolio/${value.toLowerCase()}`;
   })();
-
-  selectedItems.forEach(item => {
-    item.classList.add(isVisibleClass);
-  });
-  unselectedItems.forEach(item => {
-    item.classList.remove(isVisibleClass);
-  });
+  document.location = newLocation;
 }
